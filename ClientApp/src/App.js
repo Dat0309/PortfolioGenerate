bfa9bdc2a55ac9dff950951e2d61f2caa773f75d
Trong Hieu <anhtheboy022@gmail.com>
2023-10-31 23:10:54 +0700

    update

---
+++ ClientApp/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FullPage from './pages/FullPage/FullPage';
+
+function App() {
+  return (
+    <div className="App">
+      <FullPage />
+    </div>
+  );
+}
+
+export default App;
